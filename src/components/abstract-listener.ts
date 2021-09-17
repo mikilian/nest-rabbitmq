fe86b9aaf696b2d277deaa72cb47137effa145bd
michaelkilian <michael.kilian2406@gmail.com>
2021-09-17 17:31:07 +0200

    🏗️ Added AbstractRabbitMQListener class
    ℹ️ For more informations see https://www.rabbitmq.com/tutorials/tutorial-one-javascript.html

---
+++ src/components/abstract-listener.ts
@@ -0,0 +1,20 @@
+/**
+ * @author    Michael Kilian
+ * @copyright 2021, https://github.com/mikilian
+ * @license   MIT
+ */
+import { RabbitMQMessage } from '../types/rabbitmq-message';
+import { AbstractRabbitMQConsumer } from './abstract-consumer';
+
+export abstract class AbstractRabbitMQListener extends AbstractRabbitMQConsumer {
+  public async bind(): Promise<void> {
+    const channel = await this.connection.createChannel();
+
+    await channel.assertQueue(this.queue, { durable: false });
+    await channel.consume(this.queue, this.callback.bind(this), {
+      noAck: true,
+    });
+  }
+
+  public abstract handle(msg: RabbitMQMessage): Promise<void>;
+}
