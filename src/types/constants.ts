001a0a61adb9c11cb548475a4ef3133fec44030b
michaelkilian <michael.kilian2406@gmail.com>
2021-09-17 17:19:01 +0200

    🏗️ Added constants for the dependency injection

---
+++ src/types/constants.ts
@@ -0,0 +1,7 @@
+/**
+ * @author    Michael Kilian
+ * @copyright 2021, https://github.com/mikilian
+ * @license   MIT
+ */
+export const RABBITMQ_MODULE_OPTIONS = 'rabbitmq.module-options';
+export const RABBITMQ_CONNECTION = 'rabbitmq.connection';
